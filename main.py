460be564be8bde20b2aa90119460afc4e035b280
mmoua89 <mmoua.89@outlook.com>
2024-07-19 09:14:48 -0700

    add module1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Author: Meng Moua
 Course: CSC500-1
-Assignment: Part 1
+Assignment: Module 1, Part 1
     - Write a Python program to find the addition and subtraction of two numbers.
     - Ask the user to input two numbers (num1 and num2). Given those two numbers,
       add them together to find the output. Also, subtract the two numbers to find the output.
